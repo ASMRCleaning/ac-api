7641b3dd654a8a056904550919f6cd1a21ced2c9
Alfie Guilas <eggnoodles@Alfies-MBP.ht.home>
2023-06-12 14:06:29 -0400

    add error message for missing null token

--- src/routes/api/login.js
+++ src/routes/api/login.js
@@ -8,13 +8,19 @@ module.exports = async (req, res) => {
   try {
     const token = await User.validate(req.body.username, req.body.password);
 
-    logger.info('Login successful')
-    return res.status(200).json(createSuccessResponse({
-      token: token
-    }));
+    if (token) {
+      logger.info('Login successful')
+      return res.status(200).json(createSuccessResponse({
+        token: token
+      }));
+    } else {
+      logger.warn('POST /login error: username and password combination not found');
+      return res.status(404).json(
+        createErrorResponse(404, 'POST /login error: username and password combination not found'));
+    }
   } catch (err) {
-    logger.warn({ err }, 'POST /login error:' + err.message);
-    return res.status(404).json(
+    logger.warn({ err }, 'POST /login error: ' + err.message);
+    return res.status(500).json(
       createErrorResponse(404, err.message));
   }
 }
