9aca9f6c92f7e99737e039c937a886856eaa169c
mightycorn <alfieguilas.gmail.com>
2023-07-23 17:49:04 -0400

    modify booking constructor

--- src/routes/api/booking/index.js
+++ src/routes/api/booking/index.js
@@ -4,6 +4,15 @@ const router = express.Router();
 
 router.get('/', require('./get'));
 
+// GET /visit all visit
+// GET /visit/:id specific visit
+// GET /:bookingId/visit/:visitId specific booking with specific visit
+// PUT /visit/:id update a specific visit
+// PUT /:bookingId/visit/:visitId update a specific booking with a specific visit
+// DELETE /visit/:id delete a specific visit
+
+router.get('/:id', require('./get-id'));
+
 router.get('/all', require('./get-all'));
 
 router.post('/', require('./post'));
@@ -15,5 +24,6 @@ router.put('/:id', require('./put-id'));
 router.delete('/', require('./delete'));
 
 router.delete('/:id', require('./delete-id'));
+
 
 module.exports = router;
