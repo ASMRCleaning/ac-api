0904c419e9b6176fb248cb78988bd146064425ab
mightycorn <alfieguilas.gmail.com>
2023-07-24 17:56:56 -0400

    add PUT /booking visit routes

--- src/routes/api/booking/index.js
+++ src/routes/api/booking/index.js
@@ -4,15 +4,13 @@ const router = express.Router();
 
 router.get('/', require('./get'));
 
-// GET /visit/:id specific visit
-// GET /:bookingId/visit/:visitId specific booking with specific visit
 // PUT /visit/:id update a specific visit
 // PUT /:bookingId/visit/:visitId update a specific booking with a specific visit
 // DELETE /visit/:id delete a specific visit
 
-router.get('/visit/:id', require('./get-visit-id'));
+// router.get('/visit/:id', require('./get-visit-id'));
 
-router.get('/all', require('./get-all'));
+// router.get('/all', require('./get-all'));
 
 router.get('/:id', require('./get-id'));
 
@@ -25,6 +23,5 @@ router.put('/:id', require('./put-id'));
 router.delete('/', require('./delete'));
 
 router.delete('/:id', require('./delete-id'));
-
 
 module.exports = router;
