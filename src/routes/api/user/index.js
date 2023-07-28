e6a8f8c5a599a475a17df16ce6e4ad4da484d85e
mightycorn <alfieguilas.gmail.com>
2023-07-28 15:24:28 -0400

    add feature to change password

--- src/routes/api/user/index.js
+++ src/routes/api/user/index.js
@@ -10,4 +10,8 @@ router.get('/:id', require('./get-id'));
 
 router.put('/', require('./put'));
 
+router.put('/password', require('./put-password'));
+
+router.put('/password/:id', require('./put-password-id'));
+
 module.exports = router;
